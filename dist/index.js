670e6bf1a9646054b8efec5472b170e12f906cb8
ChaanChanChan <alexandertakepics@gmail.com>
2023-09-06 15:10:00 +0200

    scaffolding done, hello world done

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function helloNpm() {
+    return "hello NPM";
+}
+module.exports = helloNpm;
+//# sourceMappingURL=index.js.map
